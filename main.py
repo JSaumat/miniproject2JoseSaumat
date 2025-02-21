6183232a73e47baae677a3981eee5d08bacf34fe
JSaumat <j_saumat@mail.fhsu.edu>
2025-02-20 22:05:15 -0700

    Added the required header to the main.py file. Removed numpy from imported packages as it is not being used.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+'''
+
+INF601 - Programming in Python
+
+Assignment #1:  Mini Project 1
+
+I,     Jose Saumat   , affirm that the work submitted for this assignment is entirely my own.
+I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism,
+or the use of unauthorized materials. I have neither provided nor received unauthorized assistance and have
+accurately cited all sources in adherence to academic standards. I understand that failing to comply with this
+integrity statement may result in consequences, including disciplinary actions as determined by my course instructor
+and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles
+of academic integrity.
+
+'''
+
 ### INF601 - Advanced Programming in Python
 ### Jose Saumat
 ### Mini Project 2
@@ -5,7 +21,6 @@
 # Imported packages
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import requests
 
